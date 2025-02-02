1ebb867b9643f8c27460fd57547854727e7764ed
eminseo2003 <eminseo2003@naver.com>
2025-02-02 15:57:38 +0900

    오늘 페이지 구현

--- side_project/TodoApp/Views/TodayListView.swift
+++ side_project/TodoApp/Views/TodayListView.swift
@@ -8,6 +8,7 @@ struct TodayListView: View {
     
     @State private var showingAddAlarm = false
     @State private var showingSortOptions = false
+    @State private var groupingbytime = false
     
     var todos: [TodoItem] {
         let items = userLists.flatMap { $0.todos ?? [] }
@@ -18,7 +19,29 @@ struct TodayListView: View {
     let title: String = "오늘"
     
     @State private var selectedSortOption: SortOption = .manual
+    
+    var morningTodos: [TodoItem] {
+        todos.filter { getHour(from: $0.time) >= 0 && getHour(from: $0.time) < 12 }
+    }
 
+    var afternoonTodos: [TodoItem] {
+        todos.filter { getHour(from: $0.time) >= 12 && getHour(from: $0.time) < 18 }
+    }
+
+    var nightTodos: [TodoItem] {
+        todos.filter { getHour(from: $0.time) >= 18 }
+    }
+    var unscheduledTodos: [TodoItem] {
+        todos.filter { getHour(from: $0.time) == -1 }
+    }
+
+
+    // 시간을 추출하는 함수 추가
+    private func getHour(from date: Date?) -> Int {
+        guard let date = date else { return -1  }
+        return Calendar.current.component(.hour, from: date)
+    }
+    
     var body: some View {
         VStack(alignment: .leading) {
             
@@ -29,85 +52,64 @@ struct TodayListView: View {
                     .foregroundColor(listColor)
                 
             }
-            
-            List {
-                ForEach(todos) { todo in
-                    
-                    
-                    VStack(alignment: .leading) {
-                        HStack(alignment: .top) {
-                            Image(systemName: todo.isCompleted ? "circle.inset.filled" : "circle")
-                                .foregroundColor(todo.isCompleted ? listColor : .gray)
-                                .onTapGesture {
-                                    toggleCompletion(for: todo)
-                                    
-                                }
-                            
-                            VStack(alignment: .leading) {
-                                HStack {
-                                    priorityText(todo.priority)
-                                    titleText(todo)
-                                }
-                                
-                                Text(todo.memo)
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                HStack(spacing: 0) {
-                                    if let date = todo.date {
-                                        Text(formattedDate(date))
-                                            .font(.subheadline)
-                                            .foregroundColor(.gray)
-                                    }
-                                    if let time = todo.time {
-                                        Text(formattedTime(time))
-                                            .font(.subheadline)
-                                            .foregroundColor(.gray)
-                                    }
-                                    if todo.daterepeat != .none {
-                                        Image(systemName: "repeat")
-                                            .font(.subheadline)
-                                            .foregroundColor(.gray)
-                                        Text(" \(todo.daterepeat.rawValue)")
-                                            .font(.subheadline)
-                                            .foregroundColor(.gray)
-                                    }
-                                }
-                                if let location = todo.location {
-                                    ZStack {
-                                        RoundedRectangle(cornerRadius: 8)
-                                            .fill(.gray.opacity(0.2))
-                                        HStack {
-                                            Image(systemName: "location.fill")
-                                                .font(.subheadline)
-                                                .foregroundColor(.black)
-                                            
-                                            Text(location)
-                                                .font(.subheadline)
-                                                .foregroundColor(.black)
-                                            Spacer()
-                                            
-                                        }
-                                        .padding(.leading, 5)
-                                        
-                                    }
-                                    
-                                }
-                                
-                            }
-                            .padding(.leading, 8)
-                            
-                            
-                            
-                            
-                        }
-                        //.padding(.bottom, -5)
-                        
-                        
-                    }
+            if !groupingbytime {
+                List {
+                    todoListView(todos)
                 }
+                .listStyle(.plain)
+                .padding(.leading, -10)
+            } else {
+                List {
+                    
+                    Section(header: Text("오전")
+                        .font(.headline)
+                        .foregroundColor(morningTodos.isEmpty ? .gray.opacity(0.5) : .gray)
+                        .padding(.vertical, 0)
+                    ) {
+                        if !morningTodos.isEmpty {
+                            todoListView(morningTodos)
+                        } else {
+                            EmptyView().frame(height: 10)
+                        }
+                    }
+                    Section(header: Text("오후")
+                        .font(.headline)
+                        .foregroundColor(afternoonTodos.isEmpty ? .gray.opacity(0.5) : .gray)
+                        .padding(.vertical, 0)
+                    ) {
+                        if !afternoonTodos.isEmpty {
+                            todoListView(afternoonTodos)
+                        } else {
+                            EmptyView().frame(height: 10)
+                        }
+                    }
+                    Section(header: Text("오늘 밤")
+                        .font(.headline)
+                        .foregroundColor(nightTodos.isEmpty ? .gray.opacity(0.5) : .gray)
+                        .padding(.vertical, 0)
+                    ) {
+                        if !nightTodos.isEmpty {
+                            todoListView(nightTodos)
+                        }
+                    }
+                    Section(header: Text("시간 미정")
+                        .font(.headline)
+                        .foregroundColor(unscheduledTodos.isEmpty ? .gray.opacity(0.5) : .gray)
+                        .padding(.vertical, 0)
+                    ) {
+                        if !unscheduledTodos.isEmpty {
+                            todoListView(unscheduledTodos)
+                        }
+                    }
+
+                    
+                    
+                }
+                .listStyle(.plain)
+                .padding(.leading, -10)
             }
-            .listStyle(.plain)
-            .padding(.leading, -10)
+            
+            
             
             HStack {
                 Button(action: { showingAddAlarm = true }) {
@@ -139,6 +141,11 @@ struct TodayListView: View {
                         // 미리 알림 선택 기능 추가
                     }) {
                         Label("미리 알림 선택", systemImage: "checkmark.circle")
+                    }
+                    Button(action: {
+                        groupingbytime.toggle()
+                    }) {
+                        Label("시간으로 그룹화", systemImage: groupingbytime ? "checkmark" : "rectangle.grid.1x2")
                     }
                     
                     Menu {
@@ -184,6 +191,83 @@ struct TodayListView: View {
         }
         
         
+    }
+    private func todoListView(_ todos: [TodoItem]) -> some View {
+        
+        ForEach(todos) { todo in
+            
+            
+            VStack(alignment: .leading) {
+                HStack(alignment: .top) {
+                    Image(systemName: todo.isCompleted ? "circle.inset.filled" : "circle")
+                        .foregroundColor(todo.isCompleted ? listColor : .gray)
+                        .onTapGesture {
+                            toggleCompletion(for: todo)
+                            
+                        }
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
+                            priorityText(todo.priority)
+                            titleText(todo)
+                        }
+                        
+                        Text(todo.memo)
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                        HStack(spacing: 0) {
+                            if let date = todo.date {
+                                Text(formattedDate(date))
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                            if let time = todo.time {
+                                Text(formattedTime(time))
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                            if todo.daterepeat != .none {
+                                Image(systemName: "repeat")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                                Text(" \(todo.daterepeat.rawValue)")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                        if let location = todo.location {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(.gray.opacity(0.2))
+                                HStack {
+                                    Image(systemName: "location.fill")
+                                        .font(.subheadline)
+                                        .foregroundColor(.black)
+                                    
+                                    Text(location)
+                                        .font(.subheadline)
+                                        .foregroundColor(.black)
+                                    Spacer()
+                                    
+                                }
+                                .padding(.leading, 5)
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    .padding(.leading, 8)
+                    
+                    
+                    
+                    
+                }
+                //.padding(.bottom, -5)
+                
+                
+            }
+        }
     }
     private func toggleCompletion(for todo: TodoItem) {
         if let index = todos.firstIndex(where: { $0.id == todo.id }) {
